b7ede52d0f840d0874af03755017e62b42cbd5c9
Radexman <Borderlandsmaniak@gmail.com>
2024-04-09 17:43:17 +0200

    Pages & Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Heading from './components/layout/Heading';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore/Explore';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
+import Offers from './pages/Offers/Offers';
+import Profile from './pages/Profile/Profile';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="min-h-screen bg-neutral">
-      <Heading />
-    </div>
+    // @TODO Navbar
+    <Routes>
+      <Route path="/" element={<Explore />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/offers" element={<Offers />} />
+      <Route path="/profile" element={<SignIn />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      {/* @TODO Not Fund Page */}
+    </Routes>
+    // @TODO Mobile Navbar
+    // @TODO Footer
   );
 }
 
---
+++ src/pages/Offers/Offers.tsx
@@ -0,0 +1,21 @@
+function Offers() {
+  return (
+    <div className="container mx-auto p-4">
+      <header className="pb-6 text-center md:text-left">
+        <h1 className="text-3xl font-semibold uppercase tracking-wide">Offers</h1>
+      </header>
+      <main className="space-y-2">
+        <div>
+          <h2 className="text-center text-xl font-semibold md:text-left">Content</h2>
+          <p className="md:text-left1 text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi harum expedita ducimus ab inventore. Quod,
+            mollitia earum error architecto modi fugiat nulla autem doloremque iusto eligendi magni perferendis maiores
+            harum molestias optio at sit officia!
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Offers;
