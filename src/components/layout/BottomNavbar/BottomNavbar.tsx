25f8aedca78050da52fd4d10fb4f15a2fef22b76
Radexman <Borderlandsmaniak@gmail.com>
2024-04-10 21:02:24 +0200

    Mobile Bottom Nav Bahaviour

--- src/components/layout/BottomNavbar/BottomNavbar.tsx
+++ src/components/layout/BottomNavbar/BottomNavbar.tsx
@@ -1,23 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaRegCompass as ExploreIcon } from 'react-icons/fa6';
 import { IoIosPricetag as OffersIcon } from 'react-icons/io';
 import { FaUser as ProfileIcon } from 'react-icons/fa';
 
 function BottomNavbar() {
+  const location = useLocation();
+
+  const pathMathRoute = (route: string): string | undefined => {
+    if (route === location.pathname) {
+      return 'active text-primary';
+    }
+    return undefined;
+  };
+
   return (
-    <nav>
+    <nav className="block md:hidden">
       <div className="btm-nav">
-        <Link to="/" className="active">
+        <Link to="/" className={pathMathRoute('/')}>
           <button type="button">
             <ExploreIcon />
           </button>
         </Link>
-        <Link to="/offers">
+        <Link to="/offers" className={pathMathRoute('/offers')}>
           <button type="button">
             <OffersIcon />
           </button>
         </Link>
-        <Link to="/profile">
+        <Link to="/profile" className={pathMathRoute('/profile')}>
           <button type="button">
             <ProfileIcon />
           </button>
