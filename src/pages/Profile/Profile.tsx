1f2125afa20e7fee17b04bd87fc0cca3d545af52
Radexman <Borderlandsmaniak@gmail.com>
2024-04-15 13:48:05 +0200

    User Logout

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,23 +1,46 @@
-import { useEffect, useState } from 'react';
-import { User, getAuth } from 'firebase/auth';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+import { getAuth } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
 import app from '../../firebase.config';
 import ThemeToggler from '../../components/layout/ThemeToggler/ThemeToggler';
 
 function Profile() {
-  const [user, setUser] = useState<User | null>(null);
   const auth = getAuth(app);
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+
+  const [user, setUser] = useState({
+    name: auth.currentUser?.displayName,
+    email: auth.currentUser?.email,
+  });
+
+  const { name, email } = user;
+
+  const { register, control, watch, handleSubmit } = useForm({
+    mode: 'onChange',
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate('/sign-in');
+  };
+
   return (
     <div className="container mx-auto p-4">
       <header className="pb-6 text-center md:text-left">
-        <h1 className="text-3xl font-semibold uppercase tracking-wide">Profile</h1>
+        <h1 className="text-3xl font-semibold uppercase tracking-wide">My Profile</h1>
       </header>
       <main className="space-y-2">
-        <div>{user ? <h2>{user.displayName}</h2> : <h2>Not logged in</h2>}</div>
-        <ThemeToggler />
+        <div>
+          <button type="button" className="btn" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+        {/* <ThemeToggler /> */}
       </main>
+      <DevTool control={control} />
     </div>
   );
 }
